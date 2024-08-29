bd76f9a0d208f7bc5d082446e2ea178a17eb75fa
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-29 09:08:19 +0300

    instatiate two Images: enenmy and backgroundImage

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyPanel extends JPanel {
 
     final int PANEL_WIDTH = 500;
     final int PANEL_HEIGHT = 500;
+    Image enemy;
+    Image backgroundImage;
 
     MyPanel(){
 
